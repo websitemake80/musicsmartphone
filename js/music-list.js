374cbebcc9aa30bb8158258d1d5372de7463f3bf
websitemake80 <95139039+websitemake80@users.noreply.github.com>
2023-02-12 20:19:18 +0900

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    { 
+        name: "Lightness",
+        artist: "Music Material",
+        img: "music-1.jpg",
+        src: "music-1.mp3"
+    },
+    { 
+        name: "Loop",
+        artist: "Music Material",
+        img: "music-2.jpg",
+        src: "music-2.mp3"
+    },
+    { 
+        name: "Ones Way Home",
+        artist: "Music Material",
+        img: "music-3.jpg",
+        src: "music-3.mp3"
+    },
+    { 
+        name: "Refreshing Wind",
+        artist: "Music Material",
+        img: "music-4.jpg",
+        src: "music-4.mp3"
+    },
+    { 
+        name: "Transparence",
+        artist: "Music Material",
+        img: "music-5.jpg",
+        src: "music-5.mp3"
+    }
+]
